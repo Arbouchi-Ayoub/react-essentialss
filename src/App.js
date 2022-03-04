7c216bf748a1c9d77ce8281a8f7f4c379b58f9d2
yassineDevox <yassine.rassy1@gmail.com>
2022-03-04 18:44:19 +0100

    done with add new task feature

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { useState } from "react";
 import AddTask from "./components/AddTask";
 import ListTask from "./components/ListTask";
 import { TASK_DATA } from "./data/task";
+import { TaskModel } from "./model/task";
 
 function App() {
-  const [listTask, setListTask] = useState(TASK_DATA)
+  const [listTask, setListTask] = useState(TASK_DATA);
+
+  //add new task
+  const addNewTask = (titleTask) => {
+    // alert(titleTask);
+    setListTask([...listTask, new TaskModel(listTask.length + 1, titleTask)]);
+  };
+
   return (
     <>
       <div>
-        <AddTask />
+        <AddTask onAddTask={addNewTask} />
         <hr />
         <ListTask list={listTask} />
       </div>
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const AddTask = () => {
+const AddTask = ({onAddTask}) => {
+  //ref title
+  const refTitle = useRef("");
+
+  const handleClick = () => {
+    //get the input title value
+    // console.log(refTitle.current.value)
+    let titleTask = refTitle.current.value;
+    if(!titleTask) alert("empty value error 😥")
+    else{
+      //send titletask to my parent (app)
+      onAddTask(titleTask)
+      //set input value to empty
+      refTitle.current.value=""
+    }
+  };
+
   return (
     <>
       <h1 className="text-center">Add Task</h1>
@@ -9,8 +25,11 @@ const AddTask = () => {
           type="text"
           className="form-control"
           placeholder="add new task "
+          ref={refTitle}
         />
-        <button className="btn btn-success m-1">add</button>
+        <button className="btn btn-success m-1" onClick={handleClick}>
+          add
+        </button>
       </div>
     </>
   );
