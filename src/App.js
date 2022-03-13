133511d83ea31c36dc86e86417a3f61144e48845
yassineDevox <yassine.rassy1@gmail.com>
2022-03-13 20:06:30 +0100

    done with topbar html & tailwind desgin impl

--- src/App.js
+++ src/App.js
@@ -1,76 +1,40 @@
-import { useState } from "react";
-import AddTask from "./components/AddTask";
-import ListTask from "./components/ListTask";
-import { TASK_DATA } from "./data/task";
-import { TaskModel } from "./model/task";
+import React from "react";
 
-function App() {
-  const [listTask, setListTask] = useState(TASK_DATA);
-  const [listTaskBuckup, setListTaskBuckup] = useState(TASK_DATA);
-
-  //add new task
-  const addNewTask = (titleTask) => {
-    let newList =[
-      ...listTask,
-      new TaskModel(
-        (listTask[listTask.length - 1] 
-          ? listTask[listTask.length - 1].id : 0) +
-          1,
-        titleTask
-      ),
-    ]
-    // alert(titleTask);
-    setListTask(newList);
-
-    setListTaskBuckup(newList)
-  };
-
-  const deleteTaskById = (taskId) => {
-    let newListTask = listTask.filter((t) => t.id !== taskId);
-    setListTask(newListTask);
-    setListTaskBuckup(newListTask)
-  };
-
-  const editTaskById = (newTitle,taskId) => {
-    let newListTask = listTask.map(
-      t=>{
-        if(t.id===taskId)
-        {
-          t.title = newTitle
-        }
-        return t 
-      }
-    )
-    setListTask(newListTask)
-    setListTaskBuckup(newListTask)
-  };
-
-  const filterTask = (queryTitle)=>{
-    
-    if(queryTitle!=="")
-    setListTask(
-      listTaskBuckup.filter(
-        t=>t.title.includes(queryTitle)
-      )
-    )
-    else 
-    setListTask(listTaskBuckup)
-  }
-
+const App = () => {
   return (
-    <>
-      <div>
-        <AddTask onAddTask={addNewTask} />
-        <hr />
-        <ListTask
-          list={listTask}
-          onDeleteTask={deleteTaskById}
-          onEditTask={editTaskById}
-          onFilterTask={filterTask}
-        />
-      </div>
-    </>
+    <div className="container mx-auto rounded p-1 drop-shadow-xl flex justify-around items-center mt-8">
+      <h1 className="text-2xl font-bold drop-shadow-xl">Linkedin
+      </h1>
+      <ul className="flex gap-3 items-center">
+        <li className="flex items-center">
+          <a href="#" className="text-sm font-bold drop-shadow">Networks</a>
+          <span className="ml-1 p-[3.5px] rounded-lg text-[8px] bg-orange-600 text-white">+12</span>
+        </li>
+        <li className="flex items-center">
+          <a href="#" className="text-sm font-bold drop-shadow">Jobs</a>
+          <span className="ml-1 p-[3.5px] rounded-lg text-[8px] bg-orange-600 text-white">2</span>
+        </li>
+      </ul>
+      <ul className=" flex gap-3 items-center">
+      <li className="flex items-center">
+          <a href="#" className="text-sm font-bold drop-shadow">Messages</a>
+          <span className="ml-1 p-[3.5px] rounded-lg text-[8px] bg-orange-600 text-white">+30</span>
+        </li>
+        <li className="flex items-center">
+          <a href="#" className="text-sm font-bold drop-shadow">Notifications</a>
+          <span className="ml-1 p-[3.5px] rounded-lg text-[8px] bg-orange-600 text-white">5</span>
+        </li>
+        <li className="flex items-center gap-1">
+          <img
+            src="https://lh3.googleusercontent.com/a-/AOh14GhlwqhYeG93A4AyeMPthAnGKevWeFNQ6E8DPLt1LA=s192-c-rg-br100"
+            alt="avatar"
+            className="rounded w-9"
+          />
+          <i class="fa-solid fa-angle-down"></i>
+        </li>
+      </ul>
+    </div>
   );
-}
+};
 
 export default App;
--- src/model/task.js
+++
@@ -1,6 +0,0 @@
-export class TaskModel{
-    constructor(id =0,title=""){
-        this.id=id
-        this.title=title
-    }
-}
