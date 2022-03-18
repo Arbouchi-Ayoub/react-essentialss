61106480f1b54f84389e16678da82b5bd5c43b30
yassineDevox <yassine.rassy1@gmail.com>
2022-03-18 16:05:20 +0100

    json placeholder todos

--- src/App.js
+++ src/App.js
@@ -1,73 +1,34 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import ListTask from "./components/ListTask";
-import { TASK_DATA } from "./data/task";
-import { TaskModel } from "./model/task";
 
 function App() {
-  const [listTask, setListTask] = useState(TASK_DATA);
-  const [listTaskBuckup, setListTaskBuckup] = useState(TASK_DATA);
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState("");
 
-  //add new task
-  const addNewTask = (titleTask) => {
-    let newList =[
-      ...listTask,
-      new TaskModel(
-        (listTask[listTask.length - 1] 
-          ? listTask[listTask.length - 1].id : 0) +
-          1,
-        titleTask
-      ),
-    ]
-    // alert(titleTask);
-    setListTask(newList);
-
-    setListTaskBuckup(newList)
-  };
-
-  const deleteTaskById = (taskId) => {
-    let newListTask = listTask.filter((t) => t.id !== taskId);
-    setListTask(newListTask);
-    setListTaskBuckup(newListTask)
-  };
-
-  const editTaskById = (newTitle,taskId) => {
-    let newListTask = listTask.map(
-      t=>{
-        if(t.id===taskId)
-        {
-          t.title = newTitle
-        }
-        return t 
-      }
-    )
-    setListTask(newListTask)
-    setListTaskBuckup(newListTask)
-  };
-
-  const filterTask = (queryTitle)=>{
-    
-    if(queryTitle!=="")
-    setListTask(
-      listTaskBuckup.filter(
-        t=>t.title.includes(queryTitle)
-      )
-    )
-    else 
-    setListTask(listTaskBuckup)
-  }
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((r) =>{ 
+        setTasks(r.data)
+        setError("")
+      })
+      .catch((e) => {
+        // console.log(Object.keys(e.response.data));
+        setError("Error Server");
+      });
+  }, []);
 
   return (
     <>
       <div>
-        <AddTask onAddTask={addNewTask} />
+        <AddTask />
         <hr />
-        <ListTask
-          list={listTask}
-          onDeleteTask={deleteTaskById}
-          onEditTask={editTaskById}
-          onFilterTask={filterTask}
-        />
+        <ListTask list={tasks} />
+        <div className={error!=="" ? "alert alert-danger":"d-none"} >
+          {error}
+        </div>
       </div>
     </>
   );
--- src/model/task.js
+++ src/model/task.js
@@ -1,6 +1,8 @@
 export class TaskModel{
-    constructor(id =0,title=""){
+    constructor(id =0,title="",completed=false,userId=1){
         this.id=id
         this.title=title
+        this.completed=completed
+        this.userId=userId
     }
 }
